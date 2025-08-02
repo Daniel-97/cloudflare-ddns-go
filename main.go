c9f264e653245f8f86e29f430f55f8d95c46215b
daniele <zeolla.d@gmail.com>
2025-08-02 15:40:47 +0200

    create_dns_record working implementation

--- main.go
+++ main.go
@@ -2,80 +2,127 @@ package main
 
 import (
 	"bytes"
-	"errors"
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type Config struct {
-	CLOUDFLARE_EMAIL   string
-	CLOUDFLARE_API_KEY string
+	CLOUDFLARE_API_TOKEN   string
+	CLOUDFLARE_ZONE_ID     string
+	CLOUDFLARE_RECORD_NAME string
+}
+
+type CloudflareRequest struct {
+	Name    string `json:"name"`
+	TTL     int    `json:"ttl"`
+	Type    string `json:"type"`
+	Comment string `json:"comment"`
+	Content string `json:"content"`
+	Proxied bool   `json:"proxied"`
 }
 
 func main() {
 
-	config, err := load_config()
+	config := load_config()
 
+	ip, err := get_current_ip()
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(-1)
 	}
 
-	ipv4, err := get_current_ip()
+	log.Println("Your ip address is", ip)
+
+	record_id, err := cloudflare_create_dns_record(*config, ip)
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(-1)
 	}
-
-	cloudflare_overwrite_dns(*config, ipv4)
+	fmt.Println(record_id)
 }
 
-func cloudflare_overwrite_dns(config Config, ip string) {
+func get_dns_record_type(address string) string {
+	if strings.Count(address, ":") >= 2 {
+		return "AAAA"
+	} else {
+		return "A"
+	}
+}
 
-	const url = "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNS_RECORD_ID"
-	json := ""
-	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(json))
+func cloudflare_create_dns_record(config Config, address string) (record_id string, err error) {
+	url := fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records", config.CLOUDFLARE_ZONE_ID)
+	log.Println(url)
 
+	req_body := CloudflareRequest{
+		Name:    config.CLOUDFLARE_RECORD_NAME,
+		TTL:     3600,
+		Type:    get_dns_record_type(address),
+		Comment: "cloudflare-ddns-go",
+		Content: address,
+		Proxied: false,
+	}
+	log.Printf("Creating new Cloudflare dns %s record for address %s -> %s", req_body.Type, req_body.Content, req_body.Name)
+
+	json_bytes, err := json.Marshal(req_body)
 	if err != nil {
-		return
+		return "", err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(json_bytes))
+	if err != nil {
+		return "", err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("X-Auth-Email", config.CLOUDFLARE_EMAIL)
-	req.Header.Set("X-Auth-Key", config.CLOUDFLARE_API_KEY)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", config.CLOUDFLARE_API_TOKEN))
 
 	client := &http.Client{}
 	res, err := client.Do(req)
 
 	if err != nil {
-
+		return "", err
 	}
 
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	body_string := string(body)
+
 	if res.StatusCode == http.StatusOK {
+		return body_string, nil
 
 	} else {
-
+		return "", fmt.Errorf("HTTP error %d: %s", res.StatusCode, body_string)
 	}
 
 }
 
-func load_config() (*Config, error) {
+func load_config() *Config {
 	var config = Config{
-		CLOUDFLARE_EMAIL:   os.Getenv("CLOUDFLARE_EMAIL"),
-		CLOUDFLARE_API_KEY: os.Getenv("CLOUDFLARE_API_KEY"),
+		CLOUDFLARE_API_TOKEN:   os.Getenv("CLOUDFLARE_API_TOKEN"),
+		CLOUDFLARE_ZONE_ID:     os.Getenv("CLOUDFLARE_ZONE_ID"),
+		CLOUDFLARE_RECORD_NAME: os.Getenv("CLOUDFLARE_RECORD_NAME"),
 	}
 
-	if config.CLOUDFLARE_EMAIL == "" {
-		return nil, errors.New("missing 'CLOUDFLARE_EMAIL' env")
+	if config.CLOUDFLARE_API_TOKEN == "" {
+		log.Fatal("missing 'CLOUDFLARE_API_TOKEN' env")
 	}
 
-	if config.CLOUDFLARE_API_KEY == "" {
-		return nil, errors.New("missing 'CLOUDFLARE_API_KEY' env")
+	if config.CLOUDFLARE_ZONE_ID == "" {
+		log.Fatal("missing 'CLOUDFLARE_ZONE_ID' env")
 	}
 
-	return &config, nil
+	if config.CLOUDFLARE_RECORD_NAME == "" {
+		log.Fatal("missing 'CLOUDFLARE_RECORD_NAME' env")
+	}
+
+	return &config
 }
 
 func get_current_ip() (ip string, err error) {
